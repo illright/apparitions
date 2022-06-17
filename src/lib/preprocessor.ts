1c8fd24c1f6c9a99be5738687117798cdd8faf4f
Lev Chelyadinov <leva181777@gmail.com>
2022-06-17 23:39:27 +0500

    Refactor the import analysis away

--- src/lib/preprocessor.ts
+++ src/lib/preprocessor.ts
@@ -1,6 +1,9 @@
 import MagicString from 'magic-string';
 import { parse, walk } from 'svelte/compiler';
+import type { Ast } from 'svelte/types/compiler/interfaces';
 import type { PreprocessorGroup } from 'svelte/types/compiler/preprocess';
+
+const packageName = 'apparitions';
 
 interface Apparition {
 	attributes: Record<string, string>;
@@ -17,7 +20,9 @@ function stringifyApparition(apparition: Apparition) {
 	return `${jointAttributes} ${jointEventHandlers}`;
 }
 
-const apparitions: Record<string, (node: string) => Apparition> = {
+type ApparitionCreator = (node: string) => Apparition;
+
+const apparitions: Record<string, ApparitionCreator> = {
 	asButton() {
 		return {
 			attributes: {
@@ -30,27 +35,59 @@ const apparitions: Record<string, (node: string) => Apparition> = {
 	}
 };
 
-export const preprocessor: PreprocessorGroup = {
-	markup: ({ content, filename }) => {
-		if (filename === undefined || !/routes\/index.svelte/.test(filename)) {
-			return { code: content };
-		}
+/**
+ * Walk the AST of the `<script>` and `<script context="module">`, searching
+ * for imports from the pseudo-library.
+ *
+ * Removes these imports from the source code and extracts the relevant variable names.
+ */
+function identifyImportsFromApparitions(ast: Ast, s: MagicString) {
+	const namedImports = new Map<string, ApparitionCreator>();
+	let namespace: string | null = null;
 
-		const ast = parse(content, { filename });
-		const s = new MagicString(content);
-		walk(ast, {
-			enter(node, parent) {
-				if (node.type === 'ImportDeclaration' && node.source.value === 'apparitions') {
-					console.log('apparitions bye');
-					s.remove(node.start, node.end);
+	const walker: Parameters<typeof walk>[1] = {
+		enter(node) {
+			if (node.type === 'ImportDeclaration' && node.source.value === packageName) {
+				for (const specifier of node.specifiers) {
+					if (specifier.type === 'ImportNamespaceSpecifier') {
+						namespace = specifier.local.name;
+					} else if (specifier.type === 'ImportSpecifier') {
+						const importedName = specifier.imported.name;
+						const variableName = specifier.local.name;
+
+						if (importedName in apparitions) {
+							namedImports.set(variableName, apparitions[importedName]);
+						}
+					}
 				}
 
+				s.remove(node.start, node.end);
+			} else if (node.type !== 'Program' && node.type !== 'Script') {
+				this.skip();
+			}
+		}
+	};
+
+	walk(ast.instance, walker);
+	walk(ast.module, walker);
+
+	return [namedImports, namespace] as const;
+}
+
+export const preprocessor: PreprocessorGroup = {
+	markup: ({ content, filename }) => {
+		const ast = parse(content, { filename });
+		const s = new MagicString(content);
+
+		const [namedImports, namespace] = identifyImportsFromApparitions(ast, s);
+		walk(ast, {
+			enter(node, parent) {
 				if (node.type === 'Action') {
-					if (node.name in apparitions) {
+					if (namedImports.has(node.name)) {
 						s.overwrite(
 							node.start,
 							node.end,
-							stringifyApparition(apparitions[node.name](parent.name)),
+							stringifyApparition(namedImports.get(node.name)!(parent.name))
 						);
 					}
 				}
