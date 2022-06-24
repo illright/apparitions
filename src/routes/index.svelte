b360865beaebee9e59a6d25720248d02d8101fc3
Lev Chelyadinov <leva181777@gmail.com>
2022-06-24 17:50:50 +0300

    Update the expected code

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,6 @@
 <script lang="ts">
-  import { createButton } from 'apparitions';
-
-
-  // In order to remove the call to the apparition constructor, we need:
-  // - to see which values are used upon destruction
-  //   PITFALL: what if the object won't be destructed?
-  // - to see which arguments are passed to the constructor
-  // - to see which element the apparition is attached to
-  const { asButton } = createButton();
+  import UserlandButton from '../userland/button.svelte';
 </script>
 
-<h1 use:asButton>Welcome to SvelteKitd</h1>
+<UserlandButton on:press={() => console.log('hello')} />
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ src/userland/button.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import { createButton } from 'apparitions';
+
+  // In order to remove the call to the apparition constructor, we need:
+  // - to see which values are used upon destruction
+  //   PITFALL: what if the object won't be destructed?
+  // - to see which arguments are passed to the constructor
+  // - to see which element the apparition is attached to
+  const { asButton, isPressed } = createButton({ dispatch: createEventDispatcher() });
+</script>
+
+<h1 use:asButton>
+  {#if $isPressed}
+    Oooh, you touch my tra-la-la
+  {:else}
+    Welcome to SvelteKitd
+  {/if}
+</h1>
