3e5fb469e9d14205837bee20db80d290937a5703
Lev Chelyadinov <leva181777@gmail.com>
2022-06-24 17:50:36 +0300

    Fix nasty build errors

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,5 @@
+import * as path from 'path';
+import { searchForWorkspaceRoot } from 'vite';
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
@@ -5,13 +7,21 @@ import { preprocessor } from 'apparitions/preprocessor';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		preprocess(),
-		preprocessor,
-	],
+	preprocess: [preprocess(), preprocessor],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		// This needs to be here for development only, without it, Vite complains that it
+		//   cannot serve files from `./package`. I don't know why, but it works.
+		vite: {
+			server: {
+				fs: {
+					allow: [
+						path.join(searchForWorkspaceRoot(process.cwd()), 'package'),
+					]
+				}
+			}
+		}
 	}
 };
 
