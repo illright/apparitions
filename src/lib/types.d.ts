7120e0688a2e54cbca089e07ec3c7de153c36c02
Lev Chelyadinov <leva181777@gmail.com>
2022-06-28 17:37:11 +0300

    Start fresh with a new SvelteKit template

--- src/lib/types.d.ts
+++
@@ -1,9 +0,0 @@
-export interface SvelteActionOutput<ParametersType> {
-	update?: (newParameters: ParametersType) => void;
-	destroy?: () => void;
-}
-
-export type EventDispatcher<EventMap> = <EventKey extends keyof EventMap>(
-	type: EventKey,
-	detail?: EventMap[EventKey]
-) => void;
--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,15 @@
-import { resolve } from 'path';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
-  kit: {
-    target: '#svelte',
-    vite: {
-      resolve: {
-        alias: {
-          $userland: resolve('./src/userland'),
-        },
-      },
-    },
-  },
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
